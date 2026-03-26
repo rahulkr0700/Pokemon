8b3c54a17f6fe43c8331eefb6bfe0fc14f78ff66
Rahul kumar <157276729+rahoolkr@users.noreply.github.com>
2026-03-26 23:55:46 +0530

    full update

--- poke-mon/src/Components/Pokemon.jsx
+++ poke-mon/src/Components/Pokemon.jsx
@@ -4,9 +4,10 @@ import { PokemonCards } from "./PokemonCards";
 
 export const Pokemon=()=>{
     const[pokemon,setPokemon]=useState([]);
-    // const[loading,setLoading]=useState(true);
-    // const[error,setError]=useState(null);
-    const API="https://pokeapi.co/api/v2/pokemon?limit=24";
+    const[loading,setLoading]=useState(true);
+    const[error,setError]=useState(null);
+    const[search,setSearch]=useState("");
+    const API="https://pokeapi.co/api/v2/pokemon?limit=124";
       
 
     // const fetchPokemon=()=>{
@@ -36,45 +37,52 @@ export const Pokemon=()=>{
           const detailedResponses =  await Promise.all(detailedPokemonData);
           console.log(detailedResponses);
           setPokemon(detailedResponses);
-        //   setPokemon(data);
-        //   setLoading(false);
+          setLoading(false);
 
         }
         catch(error){
             console.log(error);
-            // setError(error);
-            // setLoading(false);
+            setLoading(false);
+            setError(error);
         }
     }
 
     useEffect(()=>{
         fetchPokemon();
-    },[])
-
-    // console.log(pokemon);
+    },[]);
     
-    // if(loading){
-    //     return(
-    //         <h1>Loading</h1>
-    //     )
-    // }
-    // if(error){
-    //     return(
-    //         <div>
-    //             <h1>Error:{err.message}</h1>
-    //         </div>
-    //     )
-    // }
+//   Searching
+  const searchData = pokemon.filter((curPokemon) =>
+  curPokemon.name.toLowerCase().includes(search.toLowerCase())
+);
+
+    if(loading){
+        return(
+            <div>
+                <h1>Loading....</h1>
+            </div>
+        )
+    }
+    if(error){
+        return(
+            <div>
+                <h1>Error:{err.message}</h1>
+            </div>
+        )
+    }
     return(
        <>
        <section className="container">
         <header>
             <h1>Lets Catch Pokemon</h1>
         </header>
+        <div className="pokemon-search">
+            <input type="text" placeholder="Search Pokemon" value={search} onChange={(e)=> setSearch(e.target.value)}/>
+        </div>
         <div>
             <ul className="cards">
                {
-                pokemon.map((curPokemon) => {
+                searchData.map((curPokemon) => {
                   return <PokemonCards key={curPokemon.id} pokemonData={curPokemon} />;
                 })
                }
--- poke-mon/src/Components/PokemonCards.jsx
+++ poke-mon/src/Components/PokemonCards.jsx
@@ -1,7 +1,58 @@
 export const PokemonCards=({pokemonData})=>{
       return <li className="pokemon-card">
-                 <figure>
-                   <img src="" alt="" />
-                 </figure>
-             </li>;
+    <figure className="fig-card" style={{
+    width: "100%",
+    height: "20rem", // scales with root font size
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  }}>
+     <img src={pokemonData.sprites.other.dream_world.front_default} alt={pokemonData.name} style={{
+      maxWidth: "90%",
+      maxHeight: "90%",
+      objectFit: "contain",
+    }} />
+   </figure>
+                 <h1 className="pokemon-name">{pokemonData.name}</h1>
+                 <div className="pokemon-info pokemon-highlight">
+                  <p>
+                    {
+                      pokemonData.types.map((curType)=>curType.type.name).join(", ")
+                    }
+                  </p>
+                 </div>
+                 
+      <div className="grid-three-cols">
+        <p className="pokemon-info">
+          <span> Height:</span> {pokemonData.height}
+        </p>
+        <p className="pokemon-info">
+          <span> Weight:</span> {pokemonData.weight}
+        </p>
+        <p className="pokemon-info">
+          <span> speed:</span> {pokemonData.stats[5].base_stat}
+        </p>
+      </div>
+
+      <div className="grid-three-cols">
+        <div className="pokemon-info">
+          <p>{pokemonData.base_experience}</p>
+          <span> Experience:</span>
+        </div>
+        <div className="pokemon-info">
+          <p>{pokemonData.stats[1].base_stat}</p>
+          <span>Attack:</span>
+        </div>
+        <div className="pokemon-info">
+          <p>
+            {pokemonData.abilities
+              .map((abilityInfo) => abilityInfo.ability.name)
+              .slice(0, 1)
+              .join(", ")}
+          </p>
+          <span> Abilities: </span>
+        </div>
+      </div>
+   </li>;
 };
